546b3f4bdfda83b33cd8151320886a28af02d135
Synonymous64 <prajwalurkude007@gmail.com>
2023-08-14 20:57:00 +0530

    gatsby-config

--- praj-portfolio/gatsby-config.js
+++ praj-portfolio/gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
     title: 'Prajwal Urkude',
     description:
       'Prajwal Urkude is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    siteUrl: 'https://praj-portfolio.vercel.app/', // No trailing slash allowed!
+    image: '/front.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@prajwal',
   },
   plugins: [
